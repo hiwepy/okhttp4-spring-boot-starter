de6eda4a4c0eec88edbe715854af8eb0cb130558
wandl <hiwepy@163.com>
2022-05-04 22:33:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,97 @@
+# okhttp4-spring-boot-starter
+
+Spring Boot Starter For Okhttp 4.x
+
+### 组件简介
+
+ > 基于 okhttp 4.x 的 Spring Boot Starter 实现
+ 
+### 使用说明
+
+##### 1、Spring Boot 项目添加 Maven 依赖
+
+``` xml
+<dependency>
+	<groupId>com.github.hiwepy</groupId>
+	<artifactId>okhttp4-spring-boot-starter</artifactId>
+	<version>${project.version}</version>
+</dependency>
+```
+
+##### 2、在`application.yml`文件中增加如下配置
+
+```yaml
+################################################################################################################
+###okhttp4基本配置：
+################################################################################################################
+okhttp4:
+  connect-timeout: 10s
+  read-timeout: 30s
+  write-timeout: 20s
+  log-level: BODY
+  pool:
+    # 最大空闲连接梳数量，超出该值后，连接用完后会被关闭，最多只会保留idleConnectionCount个连接数量
+    max-idle-connections: 256
+    # 最大瞬时处理连接数量
+    max-requests: 128
+    # 每个请求地址最大瞬时处理连接数量
+    max-requests-per-host: 24
+```
+
+##### 3、使用示例
+
+```java
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	
+	@Autowired
+	private OkHttpClient okHttpClient;
+	
+	@PostConstruct
+	public void test() throws IOException {
+		
+		//调用ok的get请求
+       	Request request = new Request.Builder()
+                .get()
+                .url(url)
+                .build();
+       	//同步请求方式
+	   	Response theResponse = okHttpClient.newCall(newRequest).execute();
+	   	// 解析响应内容
+	   	ResponseBody body = theResponse.body();
+	   	// 响应头信息
+	   	Headers headers = theResponse.headers();
+	   	// 响应类型
+	   	MediaType mediaType = body.contentType();
+	   	// 成功状态
+		if( theResponse.isSuccessful()) {
+			// do something
+		} 
+		
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+```
+
+## Jeebiz 技术社区
+
+Jeebiz 技术社区 **微信公共号**、**小程序**，欢迎关注反馈意见和一起交流，关注公众号回复「Jeebiz」拉你入群。
+
+|公共号|小程序|
+|---|---|
+| ![](https://raw.githubusercontent.com/hiwepy/static/main/images/qrcode_for_gh_1d965ea2dfd1_344.jpg)| ![](https://raw.githubusercontent.com/hiwepy/static/main/images/gh_09d7d00da63e_344.jpg)|
+
---
+++ src/main/java/okhttp3/spring/boot/OkHttp3AutoConfiguration.java
@@ -0,0 +1,190 @@
+package okhttp3.spring.boot;
+
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import okhttp3.*;
+import okhttp3.internal.Util;
+import okhttp3.spring.boot.ext.*;
+import okhttp3.spring.boot.ssl.SSLContexts;
+import okhttp3.spring.boot.ssl.TrustManagerUtils;
+import okhttp4.spring.boot.ext.*;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+
+import okhttp3.internal.tls.OkHostnameVerifier;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+/**
+ * OkHttp Client Ini
+ */
+@Configuration
+@ConditionalOnClass(okhttp3.OkHttpClient.class)
+@EnableConfigurationProperties({ OkHttp3Properties.class, OkHttp3PoolProperties.class, OkHttp3SslProperties.class,
+	GzipRequestProperties.class, RequestHeaderProperties.class })
+public class OkHttp3AutoConfiguration {
+
+	@Bean
+	public RequestHeaderInterceptor headerInterceptor(RequestHeaderProperties headerProperties) {
+		return new RequestHeaderInterceptor(headerProperties);
+	}
+
+	@Bean
+	public RequestRetryIntercepter requestRetryIntercepter(OkHttp3Properties properties) {
+		return new RequestRetryIntercepter(properties.getMaxRetry(), properties.getRetryInterval());
+	}
+
+	@Bean
+	public GzipRequestInterceptor gzipInterceptor(GzipRequestProperties gzipProperties) {
+		return new GzipRequestInterceptor(gzipProperties);
+	}
+
+	@Bean
+	public HttpLoggingInterceptor loggingInterceptor(OkHttp3Properties properties) {
+		HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+		loggingInterceptor.setLevel(properties.getLogLevel());
+		return loggingInterceptor;
+	}
+
+	@Bean
+	public Dispatcher dispatcher(OkHttp3PoolProperties properties) {
+		Dispatcher dispatcher = new Dispatcher();
+		dispatcher.setMaxRequests(Math.max(properties.getMaxRequests(), OkHttp3PoolProperties.DEFAULT_MAX_REQUESTS));
+		dispatcher.setMaxRequestsPerHost(Math.max(properties.getMaxRequestsPerHost(), OkHttp3PoolProperties.DEFAULT_MAX_REQUESTS_PER_ROUTE));
+		return dispatcher;
+	}
+
+	@Bean
+	public okhttp3.OkHttpClient.Builder okhttp3Builder(
+			ObjectProvider<Authenticator> authenticatorProvider,
+			ObjectProvider<CertificatePinner> certificatePinnerProvider,
+			ObjectProvider<Cache> cacheProvider,
+			ObjectProvider<ConnectionSpec> connectionSpecProvider,
+			ObjectProvider<CookieJar> cookieJarProvider,
+			ObjectProvider<Dns> dnsProvider,
+            ObjectProvider<Dispatcher> dispatcherProvider,
+			ObjectProvider<EventListener> eventListenerProvider,
+			ObjectProvider<HostnameVerifier> hostnameVerifierProvider,
+			ObjectProvider<ProxyAuthenticator> proxyAuthenticatorProvider,
+			ObjectProvider<Proxy> proxyProvider,
+			ObjectProvider<ProxySelector> proxySelectorProvider,
+			ObjectProvider<SocketFactory>  socketFactoryProvider,
+			ObjectProvider<X509TrustManager> trustManagerProvider,
+			ObjectProvider<RequestInterceptor> applicationInterceptorProvider,
+			ObjectProvider<NetworkInterceptor> networkInterceptorProvider,
+			HttpLoggingInterceptor loggingInterceptor,
+			OkHttp3Properties properties,
+			OkHttp3PoolProperties poolProperties,
+			OkHttp3SslProperties sslProperties) throws Exception {
+
+		/**
+	     * Create a new connection pool with tuning parameters appropriate for a single-user application.
+	     * The tuning parameters in this pool are subject to change in future OkHttp releases. Currently
+	     */
+    	ConnectionPool connectionPool = new ConnectionPool(poolProperties.getMaxIdleConnections(), poolProperties.getKeepAliveDuration().getSeconds(), TimeUnit.SECONDS);
+
+		List<ConnectionSpec> connectionSpecs = connectionSpecProvider.stream().collect(Collectors.toList());
+		if(Objects.isNull(connectionSpecs)){
+			connectionSpecs = Util.immutableListOf(ConnectionSpec.MODERN_TLS, ConnectionSpec.CLEARTEXT);
+		}
+
+		okhttp3.OkHttpClient.Builder builder = new OkHttpClient().newBuilder()
+				// Application Interceptors、Network Interceptors : https://segmentfault.com/a/1190000013164260
+				.authenticator(authenticatorProvider.getIfAvailable(() -> Authenticator.NONE))
+				.addInterceptor(loggingInterceptor)
+				.addNetworkInterceptor(loggingInterceptor)
+				.cache(cacheProvider.getIfAvailable())
+				.callTimeout(properties.getCallTimeout())
+				.certificatePinner(certificatePinnerProvider.getIfAvailable(() -> CertificatePinner.DEFAULT))
+				.connectionPool(connectionPool)
+				.connectTimeout(properties.getConnectTimeout())
+				.connectionSpecs(connectionSpecs)
+				.cookieJar(cookieJarProvider.getIfAvailable(() -> CookieJar.NO_COOKIES))
+				.dns(dnsProvider.getIfAvailable(() -> Dns.SYSTEM))
+				.dispatcher(dispatcherProvider.getIfAvailable(() -> new Dispatcher()))
+				.eventListener(eventListenerProvider.getIfAvailable(() -> EventListener.NONE))
+				.followRedirects(properties.isFollowRedirects())
+				.followSslRedirects(properties.isFollowSslRedirects())
+				.hostnameVerifier(hostnameVerifierProvider.getIfAvailable(() -> OkHostnameVerifier.INSTANCE))
+				.protocols(properties.getProtocols())
+				.proxy(proxyProvider.getIfAvailable())
+				.proxySelector(proxySelectorProvider.getIfAvailable(() -> ProxySelector.getDefault()))
+				.proxyAuthenticator(proxyAuthenticatorProvider.getIfAvailable(() -> ProxyAuthenticator.NONE))
+				.pingInterval(properties.getPingInterval())
+				.readTimeout(properties.getReadTimeout())
+				.retryOnConnectionFailure(properties.isRetryOnConnectionFailure())
+				.socketFactory(socketFactoryProvider.getIfAvailable(() -> SocketFactory.getDefault()))
+				.writeTimeout(properties.getWriteTimeout());
+
+		for (RequestInterceptor applicationInterceptor : applicationInterceptorProvider) {
+			builder.addInterceptor(applicationInterceptor);
+		}
+		for (NetworkInterceptor networkInterceptor : networkInterceptorProvider) {
+			builder.addNetworkInterceptor(networkInterceptor);
+		}
+		if(sslProperties.isEnabled()) {
+
+			X509TrustManager trustManager = trustManagerProvider.getIfAvailable(()-> { return TrustManagerUtils.getAcceptAllTrustManager(); });
+
+			SSLContext sslContext = SSLContexts.createSSLContext(sslProperties.getProtocol().name(), null, trustManager);
+
+			SSLSocketFactory trustedSSLSocketFactory = sslContext.getSocketFactory();
+
+			builder.sslSocketFactory(trustedSSLSocketFactory, trustManager);
+
+		}
+
+		return builder;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(OkHttpClient.class)
+	public OkHttpClient okhttp3Client(okhttp3.OkHttpClient.Builder okhttp3Builder) throws Exception {
+		return okhttp3Builder.build();
+	}
+
+	@Bean
+	public OkHttp3ClientHttpRequestFactory okHttp3ClientHttpRequestFactory(OkHttpClient okhttp3Client) {
+		return new OkHttp3ClientHttpRequestFactory(okhttp3Client);
+	}
+
+	@Bean
+	public OkHttp3Template okHttp3Template(ObjectProvider<OkHttpClient> okhttp3ClientProvider,
+										  ObjectProvider<ObjectMapper> objectMapperProvider) {
+
+		OkHttpClient okhttp3Client = okhttp3ClientProvider.getIfAvailable(() -> new OkHttpClient.Builder().build());
+
+		ObjectMapper objectMapper = objectMapperProvider.getIfAvailable(() -> {
+			ObjectMapper objectMapperDef = new ObjectMapper();
+			objectMapperDef.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+			objectMapperDef.enable(MapperFeature.USE_GETTERS_AS_SETTERS);
+			objectMapperDef.enable(MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS);
+			objectMapperDef.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+			objectMapperDef.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+			return objectMapperDef;
+		});
+
+		return new OkHttp3Template(okhttp3Client, objectMapper);
+	}
+
+}
